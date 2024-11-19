4a6b7fbc4fb4fed397d1734f4bff608f8fcfc7eb
netorapg <renatoplatz11@outlook.com>
2024-11-19 17:48:37 -0300

    estudando closure

---
+++ 14 - Funções Avançadas/14.7 - Closure/closure.go
@@ -0,0 +1,19 @@
+package main
+
+func closure() func() {
+	texto := "Dentro da função closure"
+	funcao := func() {
+		println(texto)
+	}
+
+	return funcao
+}
+
+func main() {
+	texto := "Dentro da função main"
+	println(texto)
+
+	funcaoNova := closure()
+	funcaoNova()
+
+}
