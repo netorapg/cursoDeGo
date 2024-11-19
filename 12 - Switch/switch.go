cd5ff37e328e2e94386101ef49fb6ec78ab61ae7
netorapg <renatoplatz11@outlook.com>
2024-11-19 16:19:14 -0300

    revendo switch

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	var diaDaSemana string
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough // Cai na próxima, provavelemente nunca vou usar
+	case numero == 2:
+		diaDaSemana = "Segunda-feira"
+	case numero == 3:
+		diaDaSemana = "Terça-feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número inválido"
+	}
+
+	return diaDaSemana
+}
+
+func main() {
+	fmt.Println("Switch")
+	dia := diaDaSemana(3)
+	fmt.Println(dia)
+	dia = diaDaSemana2(1)
+	fmt.Println(dia)
+}
