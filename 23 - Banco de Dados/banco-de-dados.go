c2633cf247e61d513b3b6128763ab7d6b9653d74
netorag <renatoplatz11@outlook.com>
2025-03-06 05:12:31 -0300

    aprendendo a fazer a conexao com banco de dados pelo golang

---
+++ 23 - Banco de Dados/banco-de-dados.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func main() {
+	urlConexao := "postgres://golang:golang@localhost:5432/devbook?sslmode=disable"
+	db, erro := sql.Open("pgx", urlConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão está aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
+}
